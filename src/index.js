cf80620c4960f3717da50464b294ea0fa55ae73c
vancouver29 <vancouver2990@gmail.com>
2022-03-15 00:37:23 +0100

    Chapter 19 Integrating an Existing Web Application

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 const { app, BrowserWindow } = require('electron');
-const { is } = require('electron-util');
+const { is, setContentSecurityPolicy } = require('electron-util');
+
+const config = require('./config');
 
 // to avoid garbage collection, declare the window as a variable
 let window;
+
 // specify the details of the browser window
 function createWindow() {
   window = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      // avoid the security risks of a locally running node application accessing an external site.
+      nodeIntegration: false
     }
   });
-  // load the HTML file
-  window.loadFile('index.html');
+
+  // load the URL
+  if (is.development) {
+    window.loadURL(config.LOCAL_WEB_URL);
+  } else {
+    window.loadURL(config.PRODUCTION_WEB_URL);
+  }
+  window.loadURL('http://localhost:1234');
 
   // if in development mode, open the browser dev tools
   if (is.development) {
     window.webContents.openDevTools();
+  }
+
+  // set the CSP in production mode
+  if (!is.development) {
+    setContentSecurityPolicy(`
+    default-src 'none';
+    script-src 'self';
+    img-src 'self' https://www.gravatar.com;
+    style-src 'self' 'unsafe-inline';
+    font-src 'self';
+    connect-src 'self' ${config.PRODUCTION_API_URL};
+    base-uri 'none';
+    form-action 'none';
+    frame-ancestors 'none';
+    `);
   }
 
   // when the window is closed, reset the window object
